c48ab3d59352dc922f0982d9c7c35de0475395b0
animevn <haanhth21@gmail.com>
2019-12-22 21:17:28 +0700

    Initial commit

---
+++ app/src/main/java/com/haanhgs/app/viewpagerfragmentdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.haanhgs.app.viewpagerfragmentdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
